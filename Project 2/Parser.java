fb16ccf35eef20c750166daec594fbaafda5e98b
John Samson <jsamson3@msudenver.edu>
2018-11-14 15:45:35 -0700

    All 11/14/2018 2

--- Project 2/Parser.java
+++ Project 2/Parser.java
@@ -91,7 +91,6 @@ public class Parser {
          Node second = parseFuncDefs();
          return new Node( "funcDefs", first, second, null );
       }
-
    }
 
    // <funcDef> ->  def <var> ( ) end |
@@ -105,43 +104,80 @@ public class Parser {
       Token y = lex.getNextToken();
       Token x = lex.getNextToken();
       Token z = lex.getNextToken();
+      if ( v.isKind("eof") || w.isKind("eof") || x.isKind("eof") || y.isKind("eof") || z.isKind("eof") ) {
+         return new Node("funcDef", null, null, null);
+      }
 //      System.out.println("v = " + t.getDetails());
 //      System.out.println("w = " + f.getDetails());
 //      System.out.println("y = " + y.getDetails());
 //      System.out.println("x = " + x.getDetails());
 //      System.out.println("z = " + z.getDetails());
-      if (v.getDetails().equals("def") && w.isKind("var")) {
-         if (y.getDetails().equals("(") && x.getDetails().equals(")")) {
-            if (z.getDetails().equals("end")) {
-               // def <var> ( ) end
-               return new Node("funcDef", w.getDetails(), null, null, null);
-            }
-            else {
-               lex.putBackToken(z);
-               Node second = parseStatements();
-               // def <var> ( ) <statements> end
-               return new Node ("funcDef", w.getDetails(), null, second, null);
-            }
-         }
-         else {
-            lex.putBackToken(z);
-            lex.putBackToken(x);
-            Node first = parseParams();
-            if (lex.getNextToken().toString().equals("end")) {
-               // def <var> ( <params> ) end
-               return new Node("funcDef", w.getDetails(), first, null, null);
-            }
-            else {
-               // lex.putBackToken(x);
-               Node second = parseStatements();
-               // def <var> ( <params> ) <statements> end
-               return new Node("funcDef", w.getDetails(), first, second, null);
-            }
-         }
-      }
 
-      // System.exit(1);
-      return null;
+      if (v.getDetails().equals("def")) {
+         if (w.isKind("var")) {
+//             return new Node("funcDef", w.getDetails(), null, null, null);
+             if (y.matches("single", ")")) {
+                // def <var> ( ) end |
+                if (z.matches("var", "end")) {
+                   return new Node("funcDef", w.getDetails(), null, null, null);
+                }
+                // def <var> ( ) <statements> end
+                else {
+                   Node first = parseStatements();
+                   return new Node("funcDef", w.getDetails(), first, null, null);
+                }
+             }
+             else { // <params>
+                Token a = lex.getNextToken();
+                // def <var> ( <params> ) end |
+                if (a.matches("var", "end")) {
+                   Node first = parseParams();
+                   return new Node("funcDef", w.getDetails(), first, null ,null);
+                }
+                // def <var> ( <params> ) <statements> end
+                else { // <statements>
+                   Node first = parseParams();
+                   Node second = parseStatements();
+                   return new Node("funcDef", w.getDetails(), first, null ,null);
+                }
+             }
+         }
+         else { return null; }
+      }
+      else { return null; }
+
+//      if (v.getDetails().equals("def") && w.isKind("var")) {
+//         if (y.getDetails().equals("(") && x.getDetails().equals(")")) {
+//            if (z.getDetails().equals("end")) {
+//               // def <var> ( ) end
+//               return new Node("funcDef", w.getDetails(), null, null, null);
+//            }
+//            else {
+//               lex.putBackToken(z);
+//               Node second = parseStatements();
+//               // def <var> ( ) <statements> end
+//               return new Node ("funcDef", w.getDetails(), null, second, null);
+//            }
+//         }
+//         else {
+//            lex.putBackToken(z);
+//            lex.putBackToken(x);
+//            Node first = parseParams();
+//            if (lex.getNextToken().toString().equals("end")) {
+//               // def <var> ( <params> ) end
+//               return new Node("funcDef", w.getDetails(), first, null, null);
+//            }
+//            else {
+//               // lex.putBackToken(x);
+//               Node second = parseStatements();
+//               // def <var> ( <params> ) <statements> end
+//               return new Node("funcDef", w.getDetails(), first, second, null);
+//            }
+//         }
+//      }
+//
+//      // System.exit(1);
+//      return null;
    }
 
    // <params> -> <var> | <var> , <params>
